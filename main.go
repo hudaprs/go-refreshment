2aee6bbd2ff84f39662935154a8728ba24e1b989
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2024-03-20 11:57:07 +0700

    chore: add loop

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName string = "Conference Go"
@@ -12,22 +15,31 @@ func main() {
 	fmt.Printf("We have total of %v and have still available about %v\n", totalTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName, lastName, email string
-	var userTickets uint
+	for {
+		var firstName, lastName, email string
+		var userTickets uint
 
-	fmt.Println("Enter your first name:")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name:")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email address:")
-	fmt.Scan(&email)
-	fmt.Println("How many ticket you want to book?")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email address:")
+		fmt.Scan(&email)
+		fmt.Println("How many ticket you want to book?")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thank you %v %v for booked %v ticket(s), you will receive an email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Printf("These are all our bookings %v\n", bookings)
+		fmt.Printf("Thank you %v %v for booked %v ticket(s), you will receive an email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		users := []string{}
+		for _, booking := range bookings {
+			names := strings.Fields(booking)
+			users = append(users, names[0])
+		}
+
+		fmt.Printf("These are all our bookings %v\n", users)
+	}
 }
